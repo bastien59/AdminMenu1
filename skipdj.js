19689bf52f4703570df4201a5c675f991ec41bbb
bastien59 <bastien.59139@hotmail.fr>
2013-12-14 15:14:49 +0100

    Create skipdj.js

---
+++ skipdj.js
@@ -0,0 +1,10 @@
+// Skip
+ 
+ 
+API.on(API.CHAT_COMMAND, callback);
+ 
+        function callback(value) {
+                if (value=='/skip') {
+                        API.moderateForceSkip()
+                                     }
+                                 }
