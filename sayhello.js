7d47a8c9c189cba43af0899a30e89ddfa20c5314
bastien59 <bastien.59139@hotmail.fr>
2013-12-14 15:16:47 +0100

    Create sayhello.js

---
+++ sayhello.js
@@ -0,0 +1,20 @@
+// Hello
+ 
+API.on(API.CHAT_COMMAND, callback);
+        function callback(value) {
+                if (value=='/sayhello') {
+                        sayhello();
+                        API.chatLog("Sayhello activé");
+                                     }
+                else if (value=='/stopsayhello') {
+                        API.off(API.USER_JOIN, callback);
+                        API.chatLog("Sayhello désactivé");
+                }
+                }
+ 
+function sayhello() {
+        API.on(API.USER_JOIN, callback);
+        function callback(user) {
+                API.sendChat("Salut " + "@" + user.username + " :)")
+}
+}
